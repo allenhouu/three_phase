e0889f786fc063a07e402757e6db7752ca70171c
allenhouu <126ahou@frhsd.com>
2026-03-27 10:08:35 -0400

    commit + push

---
+++ three_phase_website/src/App.tsx
@@ -0,0 +1,17 @@
+//import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+
+
+  return (
+      <>
+        <div id="welcome">Welcome to Allen Bank</div>
+        <div id="username"><input placeholder="Username"></input></div>
+        <div id="password"><input placeholder="Password"></input></div>
+      </>
+  )
+}
+
+export default App
